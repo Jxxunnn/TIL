f43f78249fde1ec0a413d8c1a2231fa438065701
Jxxunnn <roof2586@naver.com>
2022-10-15 12:28:24 +0900

    Feat: 캐러셀 슬라이드

---
+++ FE3_JS/carousel/main.js
@@ -0,0 +1,9 @@
+/* main.js */
+const container = document.querySelector(".container");
+const prevButton = document.querySelector(".btn-prev");
+const nextButton = document.querySelector(".btn-next");
+
+prevButton.addEventListener("click", () => {});
+
+nextButton.addEventListener("click", () => {});
+carousel.style.transform = `translateX(-${500 * index}px)`;
