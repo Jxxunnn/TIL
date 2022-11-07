1eee46b8f1015dce581f021ea9df7315cc5f3b0d
Jxxunnn <roof2586@naver.com>
2022-11-07 22:11:56 +0900

    Learn: component

--- react/coding-appele/blog/src/App.js
+++ react/coding-appele/blog/src/App.js
@@ -12,6 +12,8 @@ function App() {
   ]);
   let [likes, setLikes] = useState(Array(title.length).fill(0));
   let [modal, setModal] = useState(false);
+  let [idx, setIdx] = useState(0);
+  let 입력값 = "";
   return (
     <div className="App">
       <div className="black-nav">
@@ -27,12 +29,13 @@ function App() {
       >
         글제목 가나다순 정렬
       </button>
-      {title.map(function (title, i) {
+      {title.map(function (title, i, arr) {
         return (
           <div className="list" key={i}>
             <h4
               onClick={() => {
-                setModal(!modal);
+                setModal(true);
+                setIdx(i);
               }}
             >
               {title}
@@ -48,10 +51,45 @@ function App() {
               {likes[i]}
             </h4>
             <p>2월 17일 발행</p>
+            <button
+              onClick={(e) => {
+                let copy = [...arr];
+                copy.splice(i, 1);
+                console.log(copy);
+                setTitle(copy);
+              }}
+              type="button"
+            >
+              삭제
+            </button>
           </div>
         );
       })}
-      {modal === true ? <Modal title={title}></Modal> : null}
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          let copy = [...title];
+          copy.push(입력값);
+          setTitle(copy);
+          console.log(title);
+        }}
+      >
+        <input
+          onChange={(e) => {
+            입력값 = e.target.value;
+          }}
+        ></input>
+        <button type="submit">제출</button>
+      </form>
+      {modal === true ? (
+        <Modal
+          idx={idx}
+          title={title}
+          setTitle={setTitle}
+          color={"green"}
+        ></Modal>
+      ) : null}
     </div>
   );
 }
@@ -66,9 +104,20 @@ const Post = () => {
 const Modal = (props) => {
   return (
     <div className="modal">
-      <h4>{props.title}</h4>
+      <h4 style={{ color: props.color }}>{props.title[props.idx]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button
+        onClick={() => {
+          console.log(props);
+          const copy = props.title;
+          copy[0] = "구리 구리 추천";
+          props.setTitle(copy);
+        }}
+        type="button"
+      >
+        글 수정 버튼
+      </button>
     </div>
   );
 };
